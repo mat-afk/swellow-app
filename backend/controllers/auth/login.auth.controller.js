52b10fb19b4187dc71ee43adb2cfb1f2959c82f9
Mateus <mateusc.ramos@outlook.com>
2024-03-27 23:42:42 -0300

    feat: add login endpoint

--- backend/controllers/auth/login.auth.controller.js
+++ backend/controllers/auth/login.auth.controller.js
@@ -1,5 +1,35 @@
-const login = (req, res) => {
-  res.send("Login route");
+import bcrypt from "bcryptjs";
+
+import User from "../../models/user.model.js";
+import generateTokenAndSetCookie from "../../utils/generateToken.js";
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Incorrect username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    return res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePicture: user.profilePicture,
+    });
+
+  } catch (error) {
+    console.log("Error in login controller: " + error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export default login;
