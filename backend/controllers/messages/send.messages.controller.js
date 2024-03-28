8cc101ec26f8d2cb276b348ad3c178a074db080e
mat-afk <mateusc.ramos@outlook.com>
2024-03-28 18:44:49 -0300

    feat: add send message controller

---
+++ backend/controllers/messages/send.messages.controller.js
@@ -0,0 +1,41 @@
+import Conversation from "../../models/conversation.model.js";
+import Message from "../../models/message.model.js";
+
+const sendMessage = async (req, res) => {
+  try {
+    const senderId = req.user._id;
+    const { id: receiverId } = req.params;
+    const { message } = req.body;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    // Socket.io
+
+    await conversation.save();
+
+    return res.status(201).json(newMessage)
+  } catch (error) {
+    console.log("Error in sendMessage controller: " + error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export default sendMessage;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { config } from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messagesRoutes from "./routes/messages.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,8 +12,10 @@ const PORT = process.env.PORT || 5000;
 config();
 
 app.use(express.json()); // parse the requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messagesRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
