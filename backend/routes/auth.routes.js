035dc16adad4047a8d2bc150b5e84c57a31ca220
Mateus <mateusc.ramos@outlook.com>
2024-04-09 23:24:46 -0300

    style: remove space between routes declaration

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,9 +7,7 @@ import logout from "../controllers/auth/logout.auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
 export default router;
