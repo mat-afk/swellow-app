3cba9a8cdf57ad94b106555ffd6a261f4c80e2dd
Mateus <mateusc.ramos@outlook.com>
2024-04-11 12:52:47 -0300

    refactor: update favicon and background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <div className="p-4 h-screen flex items-center justify-center font-poppins bg-gradient-to-r from-amber-300 to-yellow-400">
+    <div className="p-4 h-screen flex items-center justify-center font-poppins">
       <Login />
     </div>
   );
