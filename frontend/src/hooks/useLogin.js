e28f9b475efb928c7eac44b1556c953167cee1c5
mat-afk <mateusc.ramos@outlook.com>
2024-04-17 18:18:51 -0300

    feat: add login functionality

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const login = async ({ username, password }) => {
+    const ok = handleInputErrors({ username, password });
+    if (!ok) return;
+
+    setLoading(true);
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      localStorage.setItem("user-info", JSON.stringify(data));
+      setAuthUser(data);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, login };
+};
+
+const handleInputErrors = ({ username, password }) => {
+  if (!username || !password) {
+    toast.error("Please fill in all the fields");
+    return false;
+  }
+
+  return true;
+};
+
+export default useLogin;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loading, login } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login({ username, password });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full px-8 py-6 rounded-lg shadow-md bg-zinc-100 bg-clip-padding backdrop-filter">
@@ -6,42 +20,49 @@ const Login = () => {
           Log in <span className="text-yellow-400">Swellow</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mt-3">
             <label className="label p-2">
-              <span className="text-sm label-text font-semibold">
-                Username
-              </span>
+              <span className="text-sm label-text font-semibold">Username</span>
             </label>
             <input
               type="text"
               placeholder="Enter your username"
               className="min-w-96 h-10 input input-bordered"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="mt-2">
             <label className="label">
-              <span className="text-sm label-text font-semibold">
-                Password
-              </span>
+              <span className="text-sm label-text font-semibold">Password</span>
             </label>
             <input
               type="password"
               placeholder="Enter your password"
               className="w-full h-10 input input-bordered"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
-          <a
-            href="#"
+          <Link
+            to={"/signup"}
             className="text-sm hover:underline hover:text-blue-600 mt-4 inline-block"
           >
             Don't have an account?
-          </a>
+          </Link>
 
           <div>
-            <button className="btn btn-block bg-yellow-400 hover:bg-yellow-500 btn-md font-semibold text-md mt-4">
-              Log in
+            <button
+              className="btn btn-block bg-yellow-400 hover:bg-yellow-500 btn-md font-semibold text-md mt-4"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Log in"
+              )}
             </button>
           </div>
         </form>
