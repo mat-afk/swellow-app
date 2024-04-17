3aa7605952e79585e8b157ef8c3a52c39cb2a245
mat-afk <mateusc.ramos@outlook.com>
2024-04-17 16:02:38 -0300

    feat: add protected routes

--- frontend/src/hooks/useSignUp.js
+++ frontend/src/hooks/useSignUp.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignUp = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
   const signUp = async ({
     fullName,
@@ -35,7 +37,16 @@ const useSignUp = () => {
         }),
       });
 
-      const data = res.json();
+      const data = await res.json();
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      // Saving user data
+      localStorage.setItem("user-info", JSON.stringify(data));
+      setAuthUser(data);
+
       console.log(data);
     } catch (error) {
       toast.error(error.message);
