2d42349afd19e5e289953371502d6cc29257c48e
Mateus <mateusc.ramos@outlook.com>
2024-04-18 19:04:18 -0300

    feat: add select conversation functionality

--- frontend/src/components/messages/ConversationContainer.jsx
+++ frontend/src/components/messages/ConversationContainer.jsx
@@ -1,17 +1,25 @@
+import { useEffect } from "react";
+import { conversationStore } from "../../zustand/conversationStore";
 import Conversation from "./Conversation";
 import ConversationHeader from "./ConversationHeader";
 import MessageInput from "./MessageInput";
 import NoChatSelected from "./NoChatSelected";
 
 const ConversationContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = conversationStore();
+
+  useEffect(() => {
+    // clean up
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
-          <ConversationHeader to="Luiz Quirino" />
+          <ConversationHeader to={selectedConversation.fullName} />
           <Conversation />
           <MessageInput />
         </>
--- frontend/src/components/messages/NoChatSelected.jsx
+++ frontend/src/components/messages/NoChatSelected.jsx
@@ -1,10 +1,13 @@
 import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
+
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg ms:text-xl text-zinc-900 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome, 👋 Luiz Quirino 🐦</p>
+        <p>Welcome, 👋 {authUser.fullName} 🐦</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
