9335c93cdb1cab89880e07833d7c490be09c6f2f
Mateus <mateusc.ramos@outlook.com>
2024-04-19 19:25:11 -0300

    ops: deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 config();
 
 app.use(express.json()); // parse the requests with JSON payloads (from req.body)
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/users", usersRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
